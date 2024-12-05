8551d1dc9ea93d9e232fc155944152da58720835
emmeiwhite <emmeiwhite@gmail.com>
2024-12-06 00:20:41 +0530

    react-redux-1.4 Setting up store & making it available for the entire app with Provider from react-redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import PizzaBox from './component/PizzaBox'
+import { Provider } from 'react-redux'
+import store from './component/redux/store'
 
 function App() {
   return (
-    <div className="App">
-      <PizzaBox />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <PizzaBox />
+      </div>
+    </Provider>
   )
 }
 
---
+++ src/component/redux/store.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux'
+
+import { pizzaReducer } from './pizza/PizzaReducers'
+
+const store = createStore(pizzaReducer)
+
+export default store
+// we have to provide this store to our entire application and for this we'll make use of react-redux library and use Provider Component in our App.js
