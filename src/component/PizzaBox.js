d614ac013d7fc9354f9d7d69d8acdc8f0707b37e
emmeiwhite <emmeiwhite@gmail.com>
2024-12-06 08:37:06 +0530

    react-redux-1.5 useSelector & useDispatch, fantastic

--- src/component/PizzaBox.js
+++ src/component/PizzaBox.js
@@ -1,16 +1,17 @@
 import { orderPizza } from './redux/index.js'
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
-function PizzaBox(props) {
-  console.log('******* the mapped props **********')
-  console.log(props)
+function PizzaBox() {
+  const pizzaBase = useSelector(state => state.pizzaBase)
+  const dispatch = useDispatch()
   return (
     <div>
-      <h1>Total Pizza bases: {props.pizzaBase}</h1>
+      <h1>Total Pizza bases: {pizzaBase}</h1>
       <p>Enjoy the delight of real Pizza</p>
       <button
         className="btn"
-        onClick={props.orderPizza}
+        onClick={() => dispatch(orderPizza())}
+        // dispatching action to the reducer {type:ORDER_PIZZA}
       >
         Order Now
       </button>
@@ -18,20 +19,4 @@ function PizzaBox(props) {
   )
 }
 
-// We need to access our global state in our component, for which we will use the function with the convention name as mapStateToProps
-const mapStateToProps = state => {
-  return {
-    pizzaBase: state.pizzaBase
-  }
-}
-
-// We need to dispatch our actions for which we conventionally make use of mapDispatchToProps:
-
-/** These two function maps  */
-const mapDispatchToProps = dispatch => {
-  return {
-    orderPizza: () => dispatch(orderPizza())
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(PizzaBox)
+export default PizzaBox
