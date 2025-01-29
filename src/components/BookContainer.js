314a93334a61b5c7692a4b4da73fc1a76e30ade4
emmeiwhite <emmeiwhite@gmail.com>
2025-01-29 21:13:48 +0530

    react-redux-2.3 mapStateToProps & mapDispatchToProps

--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -1,4 +1,23 @@
-const BookContainer = () => {
-  return <div>BookContainer</div>
+import { borrow_book } from '../redux-library/bookActions'
+
+const BookContainer = props => {
+  return (
+    <div>
+      <h1>Current Book Count: {props.bookCount} </h1>
+    </div>
+  )
 }
+
+const mapStateToProps = state => {
+  return {
+    bookCount: state.totalBooks
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    borrowBook: () => dispatch(borrow_book())
+  }
+}
+
 export default BookContainer
