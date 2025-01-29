a920cf1e4eeb0c6f179f379266fd989499ead889
emmeiwhite <emmeiwhite@gmail.com>
2025-01-29 23:20:57 +0530

    react-redux-2.8 composeWithDevTools - redux devtool extension

--- src/redux-library/store.js
+++ src/redux-library/store.js
@@ -2,4 +2,6 @@ import { applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
 import rootReducer from './rootReducer'
 
-export const store = createStore(rootReducer, applyMiddleware(logger))
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
