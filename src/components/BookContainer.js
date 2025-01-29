ac87181d1bb2c9a4ce092ead9a14675f6ef8541c
emmeiwhite <emmeiwhite@gmail.com>
2025-01-29 21:30:38 +0530

    react-redux-2.5 useSelector & useDispatch hooks to rescue from boilerplate code and connect() functions role

--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -1,15 +1,24 @@
-import { connect } from 'react-redux'
+// import { connect } from 'react-redux'
 import { borrow_book } from '../redux-library/bookActions'
+import { useDispatch, useSelector } from 'react-redux'
 
 const BookContainer = props => {
+  // Get state value using useSelector
+  const booksCount = useSelector(state => state.totalBooks)
+
+  // Get dispatch function
+  const dispatch = useDispatch()
   return (
     <div>
-      <h1>Current Book Count: {props.bookCount} </h1>
-      <button onClick={props.borrowBook}>Borrow Book</button>
+      <h1>Current Book Count: {booksCount} </h1>
+      <button onClick={() => dispatch(borrow_book())}>Borrow Book</button>
     </div>
   )
 }
 
+export default BookContainer
+
+/* --- This whole boilerplate code is not required
 const mapStateToProps = state => {
   return {
     bookCount: state.totalBooks
@@ -23,3 +32,4 @@ const mapDispatchToProps = dispatch => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(BookContainer)
+--- */
