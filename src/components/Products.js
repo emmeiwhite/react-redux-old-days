a710954b9c2f9a5ca6a22cf0a013dbe0cfd9c118
emmeiwhite <emmeiwhite@gmail.com>
2025-01-30 21:09:21 +0530

    react-redux-3.4 celebrating successful async fetching with thunk, really feeling proud about small successes

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,19 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProducts } from '../redux-library/productActions'
+
 const Products = () => {
+  const products = useSelector(state => state.products)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchProducts())
+  }, [])
+
+  if (products.loading) return <h2>Loading ...</h2>
+
+  if (products.error) return <h2>Error ...</h2>
+
   return (
     <article className="bg-blue-300 mt-10 p-5">
       <h1 className="text-3xl  font-semibold text-center text-green-900">Products</h1>
