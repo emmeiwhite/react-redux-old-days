fb32868cdfb3bb273bcfa2a5b28812a46a7d3947
emmeiwhite <emmeiwhite@gmail.com>
2025-02-04 15:41:01 +0530

    react-redux-5.0 dispatching actions to reducer to handle application state accordingly

--- src/quiz-app/QuizMain.js
+++ src/quiz-app/QuizMain.js
@@ -1,15 +1,45 @@
-import { useEffect } from 'react'
+import { useEffect, useReducer } from 'react'
 import axios from 'axios'
 
+const initialState = {
+  questions: [],
+
+  // 'loading','error','ready','active','finished'
+  status: 'loading'
+}
+
+function reducer(state, action) {
+  if (action.type === 'data_received') {
+    return {
+      ...state,
+      questions: action.payload,
+      status: 'ready'
+    }
+  }
+
+  if (action.type === 'data_failed') {
+    return {
+      ...state,
+      status: 'error'
+    }
+  }
+
+  throw new Error('No Action Matched ')
+}
+
 const url = 'http://localhost:9000/questions'
+
 const QuizMain = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   useEffect(() => {
     async function fetchData() {
       try {
-        const data = await axios(url)
+        const { data } = await axios(url)
         console.log(data)
+        dispatch({ type: 'data_received', payload: data })
       } catch (error) {
-        console.error('Error fetching data:', error)
+        dispatch({ type: 'data_failed' })
       }
     }
 
