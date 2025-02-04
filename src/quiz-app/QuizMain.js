b0c8abe4dca6da64e58525acd78abfb5edadeb7a
emmeiwhite <emmeiwhite@gmail.com>
2025-02-04 14:29:54 +0530

    react-redux-4.9 fetching data successfully from local server

--- src/quiz-app/QuizMain.js
+++ src/quiz-app/QuizMain.js
@@ -1,4 +1,20 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+
+const url = 'http://localhost:9000/questions'
 const QuizMain = ({ children }) => {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await axios(url)
+        console.log(data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [])
   return <main className="sm:px-16 mt-12">{children}</main>
 }
 export default QuizMain
