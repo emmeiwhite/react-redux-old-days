acfb18f14cbf8eef7058d213e2cf39592b6dc013
emmeiwhite <emmeiwhite@gmail.com>
2025-02-05 15:52:40 +0530

    react-redux-6.0 conditional styling with tailwind - which really is something to cheer about

--- src/quiz-app/Options.js
+++ src/quiz-app/Options.js
@@ -1,12 +1,14 @@
+import { useState } from 'react'
 export default function Options({ currentQuestion, dispatch }) {
   const { correctOption, options } = currentQuestion
+
+  // We'll use this to check whether the user has clicked on a particular option because we have to setup the styling accordingly
+  const [clickedOption, setClickedOption] = useState(null)
+
   const handleClick = index => {
+    setClickedOption(index)
     if (index === correctOption) {
-      console.log('The answer is correct')
       dispatch({ type: 'correct_answer', payload: currentQuestion.points })
-    } else {
-      console.log('The answer is incorrect')
-      return
     }
   }
   return (
@@ -14,7 +16,15 @@ export default function Options({ currentQuestion, dispatch }) {
       {options?.map((option, index) => {
         return (
           <li
-            className="rounded-full px-5 py-2 bg-blue-500 text-white mb-8 transition-all duration-500 ease-in-out font-semibold hover:bg-blue-400 hover:translate-x-8  cursor-pointer"
+            className={`rounded-full px-5 py-2 bg-blue-500 text-white mb-8 transition-all duration-500 ease-in-out font-semibold hover:bg-blue-400   cursor-pointer hover:translate-x-8 
+              ${index === clickedOption ? 'translate-x-8' : ''}
+            ${
+              clickedOption !== null // Only change colors after an option is clicked
+                ? index === correctOption
+                  ? 'bg-green-500 hover:bg-green-700'
+                  : 'bg-orange-400 hover:bg-orange-600'
+                : 'bg-blue-500 hover:bg-blue-400'
+            }`}
             key={option}
             onClick={() => handleClick(index)}>
             {option}
